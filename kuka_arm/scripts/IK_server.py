71768c2d1bdde647f5f6288343f382367163fa0f
Tyler <tbwolfe@live.com>
2018-09-02 22:05:40 -0400

    Added DH parameters to IK_server

--- kuka_arm/scripts/IK_server.py
+++ kuka_arm/scripts/IK_server.py
@@ -37,6 +37,18 @@ def handle_calculate_IK(req):
         # Joint twists
         alpha0, alpha1, alpha2, alpha3, alpha4, alpha5, alpha6 = symbols('alpha0:7')
 
+
+        # Create Modified DH parameters
+
+        dhParams = {alpha0: 0,      a0: 0,      d1: 0.75,   q1: q1,
+                    alpha1: -pi/2., a1: 0.35,   d2: 0,      q2: -pi/2. + q2,
+                    alpha2: 0,      a2: 1.25,   d3: 0,      q3: q3,
+                    alpha3: -pi/2., a3: -0.054, d4: 1.5,    q4: q4,
+                    alpha4: pi/2,   a4: 0,      d5: 0,      q5: q5,
+                    alpha5: -pi/2., a5: 0,      d6: 0,      q6: q6,
+                    alpha6: 0,      a6: 0,      d7: 0.303,  q7: 0}
+
+
     ### Your FK code here
     # Create symbols
 	#
